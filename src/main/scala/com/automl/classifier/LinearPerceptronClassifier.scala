cde759f2a80573a02b20fe01785cbbc67ba05a6a
deil <deil_87@mail.ru>
2018-03-04 00:38:54 +0100

    Linear perceptron. Issues with updating state of parameters.

--- src/main/scala/com/automl/classifier/LinearPerceptronClassifier.scala
+++ src/main/scala/com/automl/classifier/LinearPerceptronClassifier.scala
@@ -4,49 +4,127 @@ import org.apache.spark.ml.linalg.{DenseVector, Vector}
 import org.apache.spark.mllib.linalg.{Matrices, Vectors}
 import org.apache.spark.mllib.linalg.distributed.{IndexedRow, IndexedRowMatrix}
 import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.{DataFrame, Dataset, Row}
+import utils.LabeledVector
+import breeze.linalg.{DenseVector => BDV}
+import org.apache.spark.mllib.linalg.{Vector => VectorMLLib}
+
+import scala.util.control.Breaks._
+import scala.util.Random
 
 class LinearPerceptronClassifier {
 
-  def extractFeaturesMatrix(featuresDF: DataFrame): IndexedRowMatrix = {
+  def extractFeaturesMatrix(featuresDF: DataFrame, withBias: Boolean = false): IndexedRowMatrix = {
 
     val featuresMatrix: IndexedRowMatrix = new IndexedRowMatrix(
       featuresDF.rdd.map {
         case Row(a)  =>
           val vector = a.asInstanceOf[DenseVector]  // TODO whether it is true regardless construction of df approach?
           org.apache.spark.mllib.linalg.Vectors.fromML(vector)
-      }.zipWithIndex.map { case (v, i) => IndexedRow(i, v) })
+      }.zipWithIndex.map { case (v, i) =>
+        if(withBias)
+          IndexedRow(i, Vectors.dense(1, v.toArray: _*)) // TODO maybe not the most efficient concatenation. Look for analog of DenseVector.vertcat(a,b)
+        else
+          IndexedRow(i, v)
+      }
+    )
 
     featuresMatrix
   }
 
-  def train(df: DataFrame) = {
+  /**
+    *
+    * @param df
+    * @return vector of parameters
+    */
+  def trainIteratively(df: DataFrame): VectorMLLib = {
+    import df.sparkSession.implicits._
 
-    val featuresDF = df.select("features")
-    val numFeatures = featuresDF.first().getAs[Vector](0).size
+    val input = df.as[LabeledVector]
+    input.cache()
 
-    val featuresMatrix: IndexedRowMatrix = extractFeaturesMatrix(df)
+    val numFeatures = input.head().features.size + 1 // one for bias
 
-
-    val vectorOfParameters =  Matrices.dense(numFeatures, 1, Array(4.0, 1.0, 5.0))
-//    val activation = featuresMatrix.multiply(vectorOfParameters).rows.collect().sum //+ bias
+    var vectorOfParameters = Vectors.dense(Array.fill(numFeatures)(Random.nextDouble()))
 
 
+    val elementwiseAddition: (Array[Double], Array[Double]) => Array[Double] = { (x:Array[Double], y: Array[Double]) =>
+      require(x.length == y.length)
+      x.zip(y).map{ case(xn, yn) => xn + yn}
+    }
+
+    val elementwiseSubtraction: (Array[Double], Array[Double]) => Array[Double]  = { (x:Array[Double], y: Array[Double]) =>
+      require(x.length == y.length)
+      x.zip(y).map{ case(xn, yn) => xn - yn}
+    }
+
+    /**
+      *
+      * @ classOfExample 1 - for positive real label, 0 - for negative label
+      * @return
+      */
+    val transferFunction: (Double, Int) => ((VectorMLLib, VectorMLLib) => VectorMLLib, Boolean) = { (activation: Double, classOfExample: Int) =>
+      def add: (VectorMLLib, VectorMLLib) => VectorMLLib = (x: VectorMLLib, y: VectorMLLib) => {
+        val res = Vectors.dense(elementwiseAddition(x.toArray,y.toArray))
+        res
+      }
+      def subtr: (VectorMLLib, VectorMLLib) => VectorMLLib = (x: VectorMLLib, y: VectorMLLib) => Vectors.dense(elementwiseSubtraction(x.toArray,y.toArray))
+      def nothing: (VectorMLLib, VectorMLLib) => VectorMLLib = (x: VectorMLLib, y: VectorMLLib) => x
+
+      var misclassified = false
+      val action =
+        if (classOfExample == 1)
+          if (activation <= 0.0) {
+            misclassified = true
+            add
+          }
+          else nothing // add features vector to parameters
+        else if (activation >= 0.0) {
+          misclassified = true
+          subtr
+        }
+        else nothing // subtract features vector to parameters
+      (action, misclassified)
+    }
+
+    val calculateAction: LabeledVector => ((VectorMLLib, VectorMLLib) => VectorMLLib, Boolean) = { row: LabeledVector =>
+
+      // val activation = featuresAsBreeze.dot(vectorOfParametersAsBreeze) //TODO breeze version of .dot is not serializable and can't be used with Spark
+      // Uses netlib-native_system-osx-x86_64.jnilib
+      val activation = Matrices.dense(numFeatures, 1, Array(1, row.features.toArray:_* )).transpose.multiply(vectorOfParameters) // or we can reshape?
+
+      val activationValue = activation.values(0)
+      println("AV:" + activationValue)
+      transferFunction(activationValue, row.label.toInt)
+    }
+
+    def terminationCriteria: Boolean = {
+      val res = input.map { row =>
+        calculateAction(row)._2 // true when misclassified.
+      }
+      val ret = !res.collect().forall(_ == false)
+      ret
+    }
+
+
+    while(terminationCriteria) {
+      input.foreach { row => // TODO in original algorithm rows are taken randomly
+
+        val featuresWithBias: Array[Double] = Array(1.0, row.features.toArray:_*) //??? cast
+        val vectorOfFeaturesWithBias: VectorMLLib = Vectors.dense(featuresWithBias)
+
+        val (learningAction, _) =  calculateAction(row)
+
+        //TODO vectorOfParameters is not updated because it is on agents
+        // 1) use map and then apply all updates at once after map is finished
+        // 2) use spark streaming
+        vectorOfParameters = learningAction(vectorOfParameters, vectorOfFeaturesWithBias)
+        println(vectorOfParameters.toArray.mkString(","))
+      }
+    }
+    vectorOfParameters
   }
 
-  def transferFunction(activation: Double): Double = {
-    if(activation >= 0.0) 1.0 else 0.0
-  }
 
 
-  /*def multiply(B: Matrix): RowMatrix = {
-    val n = numCols().toInt
-    val k = B.numCols
-    require(n == B.numRows, s"Dimension mismatch: $n vs ${B.numRows}")
-
-    require(B.isInstanceOf[DenseMatrix],
-      s"Only support dense matrix at this time but found ${B.getClass.getName}.")
-
-    ...
-  }*/
 }
--- src/test/scala/com/automl/template/simple/perceptron/LinearPerceptronSuite.scala
+++ src/test/scala/com/automl/template/simple/perceptron/LinearPerceptronSuite.scala
@@ -1,27 +1,30 @@
 package com.automl.template.simple.perceptron
 
+import java.io.NotSerializableException
+
+import breeze.linalg.DenseMatrix
 import com.automl.classifier.LinearPerceptronClassifier
 import com.automl.spark.SparkSessionProvider
+import org.apache.spark.SparkException
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.linalg.distributed.{IndexedRow, IndexedRowMatrix}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.DataFrame
 import org.scalatest.{Matchers, WordSpec}
+import utils.LabeledVector
 
 class LinearPerceptronSuite extends WordSpec with Matchers with SparkSessionProvider {
   import ss.implicits._
 
-  "LinearPerceptron" should {
-
-    "be able to create matrix from features vector" in {
-
+  def fixture =
+    new {
       val dataRDD: RDD[(Int, Int, Int)] = ss.sparkContext.parallelize(
         Array(
-          (1, 2, 1),
-          (3, 4, 2),
-          (4, 5, 3),
-          (5, 6, 4)
+          (4, 2, 1),
+          (3, 4, 0),
+          (4, 5, 0),
+          (6, 3, 1)
         )
       )
 
@@ -32,22 +35,96 @@ class LinearPerceptronSuite extends WordSpec with Matchers with SparkSessionProv
         .setOutputCol("features")
 
       val preparedTrainDS = assembler.transform(dataset)
+    }
+
+
+  import breeze.linalg.{DenseVector => BDV}
+
+  def asBreeze(values: Array[Double]): BDV[Double] = new BDV[Double](values)
+
+  "LinearPerceptron" should {
+
+    "be able to create matrix from features vector" in {
+      val f = fixture
 
       val classifier = new LinearPerceptronClassifier()
-      val matrix = classifier.extractFeaturesMatrix(preparedTrainDS.select("features"))
+      val matrix = classifier.extractFeaturesMatrix(f.preparedTrainDS.select("features"))
 
       val expectedMatrix = new IndexedRowMatrix(
         ss.sparkContext.parallelize(
           Array(
-          IndexedRow(0 , Vectors.dense(1, 2)),
+          IndexedRow(0 , Vectors.dense(4, 2)),
           IndexedRow(1 , Vectors.dense(3, 4)),
           IndexedRow(2 , Vectors.dense(4, 5)),
-          IndexedRow(3 , Vectors.dense(5, 6))
+          IndexedRow(3 , Vectors.dense(6, 3))
           )
         )
       )
       matrix.rows.collect() shouldBe expectedMatrix.rows.collect()
 
+    }
+
+    "verify that DenseVector from breeze is mutable" in {
+
+      val vectorOfParameters = Vectors.dense(Array.fill(5)(1.0))
+      val vectorOfParametersBreeze = asBreeze(vectorOfParameters.toArray)
+
+      val vectorToAdd = asBreeze(Array.fill(5)(2.0))
+
+      vectorOfParametersBreeze += vectorToAdd
+
+      vectorOfParametersBreeze.data shouldBe  Array.fill(5)(3.0)
+
+    }
+
+    "be able to calculate parameters through the iterative Perceptron learning algorithm" in {
+      val f = fixture
+      val classifier = new LinearPerceptronClassifier()
+      val activation = classifier.trainIteratively(f.preparedTrainDS)
+      activation.toArray shouldBe Array.fill(5)(3.0)
+
+    }
+
+    "not be able to calculate elementwise addition with Breeze's vectors" in {
+      val f = fixture
+      import f.preparedTrainDS.sparkSession.implicits._
+
+      def asBreeze(values: Array[Double]): BDV[Double] = new BDV[Double](values)
+
+      def res = f.preparedTrainDS.as[LabeledVector].map{ row =>
+        (asBreeze(row.features.toArray) + asBreeze(row.features.toArray)).toArray.sum
+      }
+      assertThrows[SparkException] {
+        res.limit(1).collect()(0) shouldBe 42.0
+      }
+    }
+
+    "be able to calculate elementwise addition with Breeze's vectors by collecting them locally" in {
+      val f = fixture
+      import f.preparedTrainDS.sparkSession.implicits._
+
+      def asBreeze(values: Array[Double]): BDV[Double] = new BDV[Double](values)
+
+      val res = f.preparedTrainDS.as[LabeledVector].collect().map{ row: LabeledVector =>
+        (asBreeze(row.features.toArray) + asBreeze(row.features.toArray)).toArray.sum
+      }
+
+      res.mkString(",")
+    }
+
+    "not be able to calculate elementwise addition with Breeze's matrices" in {
+      val f = fixture
+      import f.preparedTrainDS.sparkSession.implicits._
+
+      def asBreezeMatrix(values: Array[Double]): DenseMatrix[Double] = new DenseMatrix(1, 1, values)
+
+
+      def res = f.preparedTrainDS.as[LabeledVector].map{ row =>
+        (asBreezeMatrix(row.features.toArray) + asBreezeMatrix(row.features.toArray)).toArray.sum
+      }
+      assertThrows[SparkException] {
+        res.limit(1).collect()(0) shouldBe 42.0
+      }
     }
 
     "be able to train itself by perceptron learning algorithm" in {
