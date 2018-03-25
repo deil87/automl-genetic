31a5172d2b585efc46d85fb6d1b9785fc137296d
deil <deil_87@mail.ru>
2018-03-25 11:09:26 +0200

    Verifying whether StandardScaler works column wise.

---
+++ src/test/scala/com/automl/spark/StandardScalerSuite.scala
@@ -0,0 +1,56 @@
+package com.automl.spark
+
+import org.apache.spark.ml.feature.{StandardScaler, VectorAssembler}
+import org.apache.spark.ml.linalg.DenseVector
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.storage.StorageLevel
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
+
+class StandardScalerSuite extends WordSpec with Matchers with BeforeAndAfterAll with SparkSessionProvider {
+
+  import ss.implicits._
+  import utils.SparkMLUtils._
+
+  val initialDataset: DataFrame = ss.sparkContext.parallelize(
+    Array(
+      (2.2 , 10.8 , 3),
+      (1.8 , 9.2 , 5),
+      (2.1 , 10.6 , 6),
+      (1.9 , 9.4 , 7)
+    )
+  ).toDF("param1", "param2", "label")
+
+  case class Test(param1: Double, param2: Double, label: Int)
+
+  initialDataset.persist(StorageLevel.MEMORY_ONLY)
+
+  val features = Array("param1", "param2")
+
+  val featuresAssembler = new VectorAssembler()
+    .setInputCols(features)
+    .setOutputCol("features")
+
+  val withFeaturesAssembled = initialDataset.applyTransformation(featuresAssembler)
+
+  "StandardScaler" should {
+
+    "scale dataset columnwise" in {
+
+
+      val scaler = new StandardScaler()
+        .setInputCol("features")
+        .setOutputCol("scaledFeatures")
+        .setWithStd(false)
+        .setWithMean(true)
+
+      val scalerModel = scaler.fit(withFeaturesAssembled)
+
+      val scaledData = scalerModel.transform(withFeaturesAssembled)
+
+      val scaledResult = scaledData.select("scaledFeatures").map(row => row.getAs[DenseVector](0).toArray(1)).toDF("param2")
+
+      scaledResult.collect().forall(row => row.getAs[Double](0) < 1) shouldBe true
+
+    }
+  }
+}
