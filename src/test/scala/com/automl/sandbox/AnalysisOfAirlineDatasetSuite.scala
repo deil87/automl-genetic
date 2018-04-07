74b3c5603a11c130bcc0ef0cc993169445ef1334
deil <deil_87@mail.ru>
2018-04-07 23:25:48 +0200

    Added description for airline dataset. Added CRSDepTime as bucketized feature.

--- src/test/scala/com/automl/sandbox/AnalysisOfAirlineDatasetSuite.scala
+++ src/test/scala/com/automl/sandbox/AnalysisOfAirlineDatasetSuite.scala
@@ -2,7 +2,7 @@ package com.automl.sandbox
 
 import com.automl.spark.SparkSessionProvider
 import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler, LabeledPoint => MLLabeledPoint}
+import org.apache.spark.ml.feature.{Bucketizer, StringIndexer, VectorAssembler, LabeledPoint => MLLabeledPoint}
 import org.apache.spark.ml.regression.RandomForestRegressor
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
 import utils.{FeatureImportanceHelper, SparkMLUtils}
@@ -10,14 +10,18 @@ import utils.{FeatureImportanceHelper, SparkMLUtils}
 class AnalysisOfAirlineDatasetSuite extends WordSpec with Matchers with BeforeAndAfterAll with SparkSessionProvider {
 
   import utils.SparkMLUtils._
+  import ss.implicits._
+  import org.apache.spark.sql.functions.count
 
   val airlineDF_allColumns = SparkMLUtils.loadParquet("src/test/resources/airline_allcolumns_sampled_100k_parquet")
   val airlineDF = airlineDF_allColumns.select("UniqueCarrier", "Month", "DayOfWeek", "Distance", "DepTime", "CRSDepTime", "DepDelay")
 
   airlineDF_allColumns.show(10)
 
-  val features = Array("Distance", "DayOfWeek")
-  val oheFeatures = Array("UniqueCarrier_ohe", "Month_ohe")
+  airlineDF_allColumns.select("DayOfWeek").groupBy($"DayOfWeek").agg(count("DayOfWeek")).show()
+
+  val features = Array("Distance")
+  val oheFeatures = Array("UniqueCarrier_ohe","DayOfWeek_ohe", "Month_ohe", "CRSDepTime_bucket_ohe")
 
   val combinedFeatures = features ++ oheFeatures
 
@@ -30,19 +34,26 @@ class AnalysisOfAirlineDatasetSuite extends WordSpec with Matchers with BeforeAn
   }
   import org.apache.spark.sql.functions.monotonically_increasing_id
 
-  import ss.implicits._
+  private val activeHoursBuckets: Array[Double] = (600.0 to 2400.0 by 100.0).toArray
+  val timeSplits = Array(0.0) ++ activeHoursBuckets
+  val timeBucketizer = new Bucketizer()
+    .setInputCol("CRSDepTime")
+    .setOutputCol("CRSDepTime_bucket")
+    .setSplits(timeSplits)
 
   val preparedAirlineDF = airlineDF
+    .applyBucketizer(timeBucketizer)
     .indexedAndOHESet(Set("UniqueCarrier"))
     .withColumnReplace("Month", $"Month" - 1 ) // For OHE we need to have zero indexed categories
-    .ohEncodedSet(Set("Month"))
+    .withColumnReplace("DayOfWeek", $"DayOfWeek" - 1 ) // For OHE we need to have zero indexed categories
+    .ohEncodedSet(Set("Month", "CRSDepTime_bucket", "DayOfWeek"))
     .applyTransformation(featuresAssembler)
     .withColumnRenamed("DepDelay", "label")
     .toDouble("label")
     .filterOutNull("label")
     .printSchema_AndContinue
     .withColumn("uniqueIdColumn", monotonically_increasing_id)
-    .showN_AndContinue(50)
+    .showN_AndContinue(500)
     .cache()
 
 
