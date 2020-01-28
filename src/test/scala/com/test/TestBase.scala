6e01e89e6825dbe540944d6d4ec0a47291285543
Andrey Spiridonov <deil_87@mail.ru>
2020-01-28 13:19:41 +0100

    AG-202: Adjusted time limit for tests.

--- src/test/scala/com/test/TestBase.scala
+++ src/test/scala/com/test/TestBase.scala
@@ -2,11 +2,11 @@ package com.test
 
 import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.concurrent.TimeLimitedTests
-import org.scalatest.time.{Seconds, Span}
+import org.scalatest.time.{Minutes, Seconds, Span}
 import org.scalatest.{Matchers, TestSuite}
 
 trait TestBase extends Matchers with LazyLogging with TimeLimitedTests{  this: TestSuite =>
 
-  val timeLimit: Span = Span(9, Seconds)
+  val timeLimit: Span = Span(9, Minutes)
 
 }
