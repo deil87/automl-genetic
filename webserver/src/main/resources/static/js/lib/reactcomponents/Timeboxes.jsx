b656b41ac897a376766c2ad156fbd13b477c0d7b
Andrey Spiridonov <deil_87@mail.ru>
2020-02-14 13:29:31 +0100

    AG-236: added requirejs with its ability to work with modules

---
+++ webserver/src/main/resources/static/js/lib/reactcomponents/Timeboxes.jsx
@@ -0,0 +1,60 @@
+define(function(require){
+
+var d3Array = require('d3-array');
+var d3Selection = require('d3-selection');
+var d3Scale = require('d3-scale');
+
+class Timeboxes extends React.Component {
+   constructor(props){
+      super(props)
+      this.createTBProgressBar = this.createTBProgressBar.bind(this)
+   }
+   componentDidMount() {
+      this.createTBProgressBar()
+   }
+   componentDidUpdate() {
+      this.createTBProgressBar()
+   }
+
+   createTBProgressBar() {
+      const node = this.node
+      const dataMax = d3Array.max(this.props.data)
+      const yScale = d3Scale.scaleLinear()
+         .domain([0, dataMax])
+         .range([0, this.props.size[1]])
+
+      d3Selection.select(node)
+         .selectAll('rect')
+         .data(this.props.data)
+         .enter()
+         .append('rect')
+
+      d3Selection.select(node)
+         .selectAll('rect')
+         .data(this.props.data)
+         .exit()
+         .remove()
+
+      d3Selection.select(node)
+         .selectAll('rect')
+         .data(this.props.data)
+         .style('fill', '#fe9922')
+         .attr('x', (d,i) => i * 25)
+         .attr('y', d => this.props.size[1] - yScale(d))
+         .attr('height', d => yScale(d))
+         .attr('width', 25)
+   }
+
+   render() {
+      return <svg ref={node => this.node = node}
+      width={500} height={500}>
+      </svg>
+   }
+}
+
+const timeboxesReactElement = <Timeboxes data={[5,10,1,3, 4,15,9]} size={[500,500]}/>;
+
+const tableDOMContainer = document.getElementById('timeboxes_react');
+ag_global_vars.timeboxesRef = ReactDOM.render(timeboxesReactElement, tableDOMContainer);
+
+});
